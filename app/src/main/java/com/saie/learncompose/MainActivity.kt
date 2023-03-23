f346d8d3c86eada8d0b2d7a088bc7239998db472
Sai Emani <semani@yelp.com>
2023-03-23 10:00:34 -0400

    Updating message function to take a message object

--- app/src/main/java/com/saie/learncompose/MainActivity.kt
+++ app/src/main/java/com/saie/learncompose/MainActivity.kt
@@ -16,18 +16,21 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(name = "Android")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Text(text = msg.author)
+    Text(text = msg.body)
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(name = "Android")
+    MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose"))
 }
