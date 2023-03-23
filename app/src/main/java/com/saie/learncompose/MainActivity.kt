8fc3090346403d8b23f5021e8014b4188c2d8af9
Sai Emani <semani@yelp.com>
2023-03-23 10:30:53 -0400

    Adding material design and colors

--- app/src/main/java/com/saie/learncompose/MainActivity.kt
+++ app/src/main/java/com/saie/learncompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
@@ -21,7 +22,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            LearnComposeTheme {
+                androidx.compose.material.Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Android", "Jetpack Compose"))
+                }
+            }
         }
     }
 }
@@ -37,17 +42,21 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant
+            )
 
             // Add a vertical space between the author and the message
             Spacer(modifier = Modifier.height(4.dp))
-            
+
             Text(text = msg.body)
         }
     }
@@ -57,5 +66,9 @@ fun MessageCard(msg: Message) {
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose"))
+    LearnComposeTheme {
+        androidx.compose.material.Surface(modifier = Modifier.fillMaxSize()) {
+            MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose"))
+        }
+    }
 }
