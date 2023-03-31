0089efdb009ce66e7964c0925f7a0690f7ea96cc
Sai Emani <semani@yelp.com>
2023-03-30 21:23:25 -0400

    Material3 added, basic layout working

--- app/src/main/java/com/saie/learncompose/BasicActivity.kt
+++ app/src/main/java/com/saie/learncompose/BasicActivity.kt
@@ -3,13 +3,18 @@ package com.saie.learncompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.saie.learncompose.ui.theme.LearnComposeTheme
 
 class BasicActivity : ComponentActivity() {
@@ -17,27 +22,46 @@ class BasicActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LearnComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
+private fun MyApp(modifier: Modifier = Modifier,
+                  names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    //Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+
+            ElevatedButton(onClick = {}) {
+                Text(text = "Show More")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LearnComposeTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
