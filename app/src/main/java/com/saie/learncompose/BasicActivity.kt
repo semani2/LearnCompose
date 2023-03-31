09f6331dcb331c3d8d20db1784fead01db1f2009
Sai Emani <semani@yelp.com>
2023-03-30 21:53:34 -0400

    Adding spring animation for expanding.

--- app/src/main/java/com/saie/learncompose/BasicActivity.kt
+++ app/src/main/java/com/saie/learncompose/BasicActivity.kt
@@ -3,6 +3,9 @@ package com.saie.learncompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -83,7 +86,13 @@ fun Greeting(name: String) {
         mutableStateOf(false)
     }
 
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = androidx.compose.material3.MaterialTheme.colorScheme.primary,
@@ -93,7 +102,7 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
                 Text(text = name)
