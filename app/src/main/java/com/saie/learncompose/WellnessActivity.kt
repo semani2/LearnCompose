6d5949fa99e7f67c23f7f849edc1f731a1aefbbd
Sai Emani <semani@yelp.com>
2023-04-03 14:56:48 -0400

    Adding wellnesstaskitem

--- app/src/main/java/com/saie/learncompose/WellnessActivity.kt
+++ app/src/main/java/com/saie/learncompose/WellnessActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
@@ -42,16 +43,32 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         var count by remember { mutableStateOf(0) }
 
         if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today",
+                    onClose = { showTask = false })
+            }
+
             Text(
                 text = "You've had $count glasses."
             )
         }
 
-        Button(
-            onClick = { count++ },
-            Modifier.padding(top = 8.dp),
-            enabled = count < 10) {
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(
+                onClick = { count++ },
+                enabled = count < 10) {
                 Text(text = "Add one")
+            }
+
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 8.dp)) {
+                Text(text = "Clear water count")
+            }
+
         }
     }
 
